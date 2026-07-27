61ab22bb5e834af952a40105462f9220c7bd243a
suhana065 <suhanamagar065@gmail.com>
2026-07-27 08:38:22 +0545

    “push”

---
+++ mathematics.js
@@ -0,0 +1,35 @@
+// let fullname = "Ram Thapa";
+// console.log("Hello, "+ fullname);
+let ward_num = 20;
+const PI = 3.14;
+console.log("the value of PI is " + PI);
+var isTomorrowAHoliday = false;
+// fullname = "Sita Chaudhary";
+// console.log("Hello, " + fullname);
+
+let num1 = parseInt(prompt("Enter first number"));
+let num2 = parseInt(prompt("Enter second number"))
+
+//Basic arithematic operations
+
+let sum = num1 +num2;
+let diff = num1 -num2;
+let product = num1 *num2;
+let div = num1 /num2;
+let modulos = num1 %num2;
+
+
+console.log("the sum of " + num1 + " and " + num2 + " is "+ sum+".");
+console.log("the diff of " + num1 + " and " + num2 + " is "+ div+".");
+console.log("the product of " + num1 + " and " + num2 + " is "+ product+".");
+console.log("the div of " + num1 + " and " + num2 + " is "+ div+".");
+console.log("the modulos of " + num1 + " and " + num2 + " is "+ modulos+".");
+
+
+
+
+
+alert("welcome to my website!");
+let answer = confirm("Are you sure you want to continue")
+let fullname = prompt("Enter your fullname");
+alert(" welcome " + fullname)
